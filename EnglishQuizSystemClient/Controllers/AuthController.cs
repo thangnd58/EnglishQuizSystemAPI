b01087baf99cf63a2ac51ed31c678f2f07e160df
Nguyen Dinh Thang <thangga05082001@gmail.com>
2023-06-11 02:45:42 +0700

    commit

--- EnglishQuizSystemClient/Controllers/AuthController.cs
+++ EnglishQuizSystemClient/Controllers/AuthController.cs
@@ -22,30 +22,37 @@ namespace EnglishQuizSystemClient.Controllers
                 UserName = username,
                 Password = password
             };
-            using (HttpClient client = new HttpClient())
+            try
             {
-                using (HttpResponseMessage res = await client.PostAsJsonAsync(apiLink, auth))
-                {
-                    using (HttpContent content = res.Content)
-                    {
-                        if (res.IsSuccessStatusCode)
-                        {
-                            string data = await content.ReadAsStringAsync();
-                            // Deserialize the JSON response
-                            JObject jsonData = JsonConvert.DeserializeObject<JObject>(data);
-                            // Access properties of the JSON object as needed
-                            string token = jsonData["token"].ToString();
-                            HttpContext.Session.SetString("token", token);
-                            return RedirectToAction("Index", "Home");
-                        }
-                        else
-                        {
-                            ViewBag.Message = "Invalid user name or password";
-                            return View();
-                        };
-                    }
-                }
-            }
+				using (HttpClient client = new HttpClient())
+				{
+					using (HttpResponseMessage res = await client.PostAsJsonAsync(apiLink, auth))
+					{
+						using (HttpContent content = res.Content)
+						{
+							if (res.IsSuccessStatusCode)
+							{
+								string data = await content.ReadAsStringAsync();
+								// Deserialize the JSON response
+								JObject jsonData = JsonConvert.DeserializeObject<JObject>(data);
+								// Access properties of the JSON object as needed
+								string token = jsonData["token"].ToString();
+								HttpContext.Session.SetString("token", token);
+								return RedirectToAction("Index", "Home");
+							}
+							else
+							{
+								ViewBag.Message = "Invalid user name or password!";
+								return View();
+							};
+						}
+					}
+				}
+			} catch (Exception ex)
+            {
+				ViewBag.Message = "Error server!";
+				return View();
+			}
         }
 
         public IActionResult Register()
