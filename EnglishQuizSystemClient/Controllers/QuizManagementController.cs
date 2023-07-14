c227efeaa3cd11bf3f434987c8024e76478af8fa
Nguyen Dinh Thang <thangga05082001@gmail.com>
2023-07-15 01:07:02 +0700

    upload

---
+++ EnglishQuizSystemClient/Controllers/QuizManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishQuizSystemClient.Controllers
+{
+	public class QuizManagementController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
